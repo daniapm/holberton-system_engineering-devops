e6737dc95f548d841d22e9a0ca3629b5417fd6be
daniapm <daniajibellis@hotmail.com>
2021-09-13 16:46:10 -0500

    fix code

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,21 +4,19 @@ import sys
 import json
 
 if __name__ == '__main__':
-    url_emp = 'https://jsonplaceholder.typicode.com/users/{}'\
-    .format(sys.argv[1])
-    url_task = 'https://jsonplaceholder.typicode.com/todos?userId={}'\
-    .format(sys.argv[1])
+    url_emp = 'https://jsonplaceholder.typicode.com/users/{}'.
+    format(sys.argv[1])
+    url_task = 'https://jsonplaceholder.typicode.com/todos?userId={}'.
+    format(sys.argv[1])
     response_emp = requests.get(url_emp)
     employee = response_emp.json()
     response_task = requests.get(url_task)
     tasks = response_task.json()
-    count = 0
-    tittle = []
+    list_task = []
     for task in tasks:
-        if task['completed']:
-            count += 1
-            tittle.append(task['title'])
-    print('Employee {} is done with tasks({}/{}):'
-          .format(employee['name'], count, len(tasks)))
-    for element in tittle:
-        print("\t ", element)
+        if task.get('completed') is True:
+            list_task.append(task.get('title'))
+
+    print("Employee {} is done with tasks({}/{}):".
+          format(employee.get('name'), len(list_task), len(tasks)))
+    print("\n".join("\t {}".format(task) for task in list_task))
