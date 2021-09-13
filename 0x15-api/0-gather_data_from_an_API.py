d4ad5b6b9d18542a3d6d2eac99aa6308888081f4
daniapm <daniajibellis@hotmail.com>
2021-09-13 16:50:43 -0500

    fix code

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -7,10 +7,10 @@ import requests
 import sys
 
 if __name__ == '__main__':
-    url_emp = 'https://jsonplaceholder.typicode.com/users/{}'.
-    format(sys.argv[1])
-    url_task = 'https://jsonplaceholder.typicode.com/todos?userId={}'.
-    format(sys.argv[1])
+    url_emp = 'https://jsonplaceholder.typicode.com/users/{}'\
+              .format(sys.argv[1])
+    url_task = 'https://jsonplaceholder.typicode.com/todos?userId={}'\
+               .format(sys.argv[1])
     response_emp = requests.get(url_emp)
     employee = response_emp.json()
     response_task = requests.get(url_task)
