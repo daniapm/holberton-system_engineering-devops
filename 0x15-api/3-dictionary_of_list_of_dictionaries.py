e633642f97beab8eb28064cff63a24612c8324f5
daniapm <daniajibellis@hotmail.com>
2021-09-13 20:56:35 -0500

    fix code

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -12,16 +12,15 @@ if __name__ == '__main__':
     response_task = requests.get(url_task).json()
     response_emp = requests.get(url_emp).json()
 
-    list = []
     task_dict = {}
     js = {}
     for users in response_emp:
+        list = []
         for task in response_task:
             if task.get('userId') == users.get('id'):
                 task_dict["username"] = users.get('username')
                 task_dict["task"] = task.get('title')
                 task_dict["completed"] = task.get('completed')
-                id = task.get('userId')
                 list.append(task_dict)
         js[users.get('id')] = list
     with open('todo_all_employees.json', 'w') as jsonfile:
