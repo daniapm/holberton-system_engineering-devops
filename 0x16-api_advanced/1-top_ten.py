8e90efef3a86ea528bcbf68c87698ec26d6e684d
daniapm <daniajibellis@hotmail.com>
2021-09-27 14:08:40 -0500

    Funtion top_ten that return top hot post

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+function that queries the Reddit API and returns the
+number of subscribers (not active users, total subscribers)
+for a given subreddit. If an invalid subreddit is given, the
+function should return 0.
+"""
+import requests
+
+
+def top_ten(subreddit):
+    """
+    Funtion top_ten that return top hot post
+    """
+
+    url = "https://old.reddit.com/r/{}/hot.json?limit=10".format(
+        subreddit)
+    headers = {'User-Agent': 'user_agent'}
+    page = requests.get(url, headers=headers, allow_redirects=False)
+
+    if page.status_code == 200:
+        tittle = page.json()['data']['children']
+        for post in tittle:
+            print(post['data']['title'])
+    return print("None")
