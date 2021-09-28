b811778356f95353107125e483a9a1de0e69f585
daniapm <daniajibellis@hotmail.com>
2021-09-27 20:00:59 -0500

    recursive

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,26 +1,29 @@
 #!/usr/bin/python3
-""" recursive function that queries the Reddit API and
+"""
+recursive function that queries the Reddit API and
 returns a list containing the titles of all hot articles
 for a given subreddit. If no results are found for the
 given subreddit, the function should return None.
 """
+import json
 import requests
 
 
 def recurse(subreddit, hot_list=[], after=""):
-
-    url = "https://old.reddit.com/r/{}/hot.json".format(subreddit)
     headers = {'User-Agent': 'user_agent'}
-    arg = {"after": after}
-    page = requests.get(url, headers=headers, params=arg,
-                        allow_redirects=False)
-    tittle = page.json()['data']['children']
-    after = page.json()['data']['after']
+    url = "https://www.reddit.com/r/{}/hot/.json?after={}".format(
+        subreddit, after)
+    page = requests.get(url, headers=headers)
+    if page.status_code == 200:
+        tittle = page.json()
+        request = tittle.get('data').get('children')
+        pagination = tittle.get('data').get('after')
+        for titt in request:
+            hot_list.append(titt.get("data").get("title"))
+        if pagination is not None:
+            return recurse(subreddit, hot_list,  pagination)
+        else:
+            return hot_list
 
-    if tittle is not None:
-        for item in tittle:
-            hot_list.append(item.get("data").get("title"))
-    if after is not None:
-        recurse(subreddit, hot_list, after)
     else:
-        return print("None")
+        return None
