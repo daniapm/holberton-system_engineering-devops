57a6546e6636c40bf45f865b785ae852e6ca785f
daniapm <daniajibellis@hotmail.com>
2021-10-07 11:21:13 -0500

    limit

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,3 +1,4 @@
 # Change the OS configuration so that it is
 # possible to login with the holberton user and open a file without any error message.
+exec { '/usr/bin/env sed -i 's/5/4000/' /etc/security/limits.conf': }
 exec { '/usr/bin/env sed -i 's/4/2000/' /etc/security/limits.conf': }
